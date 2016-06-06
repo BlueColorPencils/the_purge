1e80bb82bb1c8d9072d4c0aaaf5dd9e776077727
Cristal Tay <bluecolorpencils@gmail.com>
2016-06-05 19:13:40 -0700

    restarted the whole project... started rails. Facebook login now effin works. wheeeeeeee

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ActiveRecord::Base
+  has_many :favorite_names # just the 'relationships'
+  has_many :favorites, through: :favorite_names, source: :name # the actual names a user favorites
+
+
+  def self.find_or_create_from_omniauth(auth_hash)
+    user = self.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+    if !user.nil?
+      User.update(oauth_token: auth_hash["credentials"]["token"])
+      User.update(oauth_expires_at: Time.at(auth_hash["credentials"]["expires_at"]))
+      return user
+    else
+      user = User.new
+      user.uid = auth_hash["uid"]
+      user.provider = auth_hash["provider"]
+      user.name = auth_hash["info"]["name"]
+      user.picture = auth_hash["info"]["image"]
+      user.oauth_token = auth_hash["credentials"]["token"]
+      user.oauth_expires_at = Time.at(auth_hash["credentials"]["expires_at"])
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
+  end
+
+
+end
